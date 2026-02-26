2f8468a869f89556bcd30e8bd5c098e7663e80b3
John Mortimore <jgmortim@mtu.edu>
2026-02-26 11:50:01 -0500

    Even better cli args

--- src/main/java/com/github/jgmortim/mornary/Mornary.java
+++ src/main/java/com/github/jgmortim/mornary/Mornary.java
@@ -3,10 +3,14 @@ package com.github.jgmortim.mornary;
 import com.github.jgmortim.mornary.service.MornaryService;
 import com.github.jgmortim.mornary.utility.AsciiUtility;
 import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 /**
@@ -14,28 +18,28 @@ import java.util.concurrent.Callable;
  *
  * @author John Mortimore
  */
-@CommandLine.Command(name = "mornary", mixinStandardHelpOptions = true, version = "mornary 1.0.0-SNAPSHOT",
+@Command(name = "mornary", mixinStandardHelpOptions = true, version = "mornary 1.0.0-SNAPSHOT",
         description = "Disguises text and binary data as Morse code")
 public class Mornary implements Callable<Integer> {
 
-    @CommandLine.ArgGroup(multiplicity = "1")
+    @ArgGroup(multiplicity = "1")
     Mode mode;
 
     static class Mode {
-        @CommandLine.Option(names = "/e", description = "Encode text", paramLabel = "<text>")
+        @Option(names = "/e", description = "Encode text", paramLabel = "<text>")
         String encodeText;
 
-        @CommandLine.Option(names = "/E", description = "Encode file", paramLabel = "<file>")
-        File encodeFile;
-
-        @CommandLine.Option(names = "/d", description = "Decode text", paramLabel = "<text>")
+        @Option(names = "/d", description = "Decode text", paramLabel = "<text>")
         String decodeText;
 
-        @CommandLine.Option(names = "/D", description = "Decode file", paramLabel = "<file>")
-        File decodeFile;
+        @Option(names = "/E", arity = "2", description = "Encode file", paramLabel = "<inputFile> <outputFile>")
+        List<File> encodeFile;
+
+        @Option(names = "/D", arity = "2", description = "Decode file", paramLabel = "<inputFile> <outputFile>")
+        List<File> decodeFile;
     }
 
-    @CommandLine.Option(names = "-n", hidden = true)
+    @Option(names = "-n", hidden = true)
     int numMatchesBeforeSelection = 10;
 
     @Override
@@ -43,32 +47,25 @@ public class Mornary implements Callable<Integer> {
 
         MornaryService service = new MornaryService();
 
-        if (mode.encodeText != null) {
-            String binary = AsciiUtility.toAsciiBinary(this.mode.encodeText);
+
+        if (this.mode.encodeText != null) {
+            String binary = AsciiUtility.toAsciiBinary(mode.encodeText);
             String output = service.binaryToMorseCode(binary, numMatchesBeforeSelection);
             System.out.println(output);
-        } else if (mode.encodeFile != null) {
-
-            String morseCode = service.toMorseCode3(this.mode.encodeFile.toURI().toURL());
-
-            //TODO a lot of this is just here for testing because I haven't make any unit tests yet
-            System.out.println(morseCode);
-//            System.out.println("Decoded as Morse:");
-//            System.out.println(MorseCode.convertToText(morseCode.replace("/", " ")));
-//            System.out.println("Decoded:");
-//            String binary = service.morseCodeToBinary(morseCode);
-//            String ascii = AsciiUtility.toAsciiText(binary);
-//            System.out.println(ascii);
-//            System.out.println("Matches: " + original.equals(ascii));
-        } else if (mode.decodeText != null) {
-            final String binary = service.morseCodeToBinary(this.mode.decodeText);
+        } else if (this.mode.decodeText != null) {
+            final String binary = service.morseCodeToBinary(mode.decodeText);
             final String output = AsciiUtility.toAsciiText(binary);
             System.out.println(output);
-        } else if (mode.decodeFile != null) {
-            final String morseCode = Files.readString(Path.of(this.mode.decodeFile.toURI()));
+        } else if (this.mode.encodeFile != null) {
+            String morseCode = service.toMorseCode3(this.mode.encodeFile.get(0).toURI().toURL());
+            Path outputPath = this.mode.encodeFile.get(1).toPath();
+            Files.writeString(outputPath, morseCode);
+        } else if (this.mode.decodeFile != null) {
+            final String morseCode = Files.readString(Path.of(this.mode.decodeFile.get(0).toURI()));
             final String binary = service.morseCodeToBinary(morseCode);
             final String output = AsciiUtility.toAsciiText(binary);
-            System.out.println(output);
+            Path outputPath = this.mode.decodeFile.get(1).toPath();
+            Files.writeString(outputPath, output);
         }
 
         return 0;
