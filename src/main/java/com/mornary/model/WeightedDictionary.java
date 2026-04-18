2b7ef30c62e39e032735d8e4f41c99c663a4259a
John Mortimore <jgmortim@mtu.edu>
2026-04-18 11:41:18 -0400

    Convert weighted dictionary to record

--- src/main/java/com/mornary/model/WeightedDictionary.java
+++ src/main/java/com/mornary/model/WeightedDictionary.java
@@ -1,30 +1,13 @@
 package com.mornary.model;
 
-import lombok.Getter;
-
 /**
  * A dictionary and the weight multiplier to apply to the score of matching words found in the dictionary.
  *
+ * @param filename                The name of the file containing the raw dictionary.
+ * @param maxMatchesForDictionary Max number of matches to find while searching the dictionary
+ * @param scoreMultiplier         The multiplier to apply to the score of words found in this dictionary.
+ *
  * @author John Mortimore
  */
-@Getter
-public class WeightedDictionary {
-
-
-    private final String filename;
-    private final double scoreMultiplier;
-    private final int maxMatchesForDictionary;
-
-    /**
-     * Constructs a new weighted dictionary.
-     *
-     * @param filename                The name of the file containing the raw dictionary.
-     * @param maxMatchesForDictionary Max number of matches to find while searching the dictionary
-     * @param scoreMultiplier         The multiplier to apply to the score of words found in this dictionary.
-     */
-    public WeightedDictionary(String filename, int maxMatchesForDictionary, double scoreMultiplier) {
-        this.filename = filename;
-        this.maxMatchesForDictionary = maxMatchesForDictionary;
-        this.scoreMultiplier = scoreMultiplier;
-    }
+public record WeightedDictionary(String filename, int maxMatchesForDictionary, double scoreMultiplier) {
 }
